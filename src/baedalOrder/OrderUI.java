7c67376c009d3eefb682d30080933882fca6d796
HB <HB@HB-PC>
2015-10-26 18:16:04 +0900

    asdf

--- src/baedalOrder/OrderUI.java
+++ src/baedalOrder/OrderUI.java
@@ -1,5 +1,91 @@
 package baedalOrder;
 
-public class OrderUI {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class OrderUI extends JFrame implements ActionListener, Runnable{
+
+	private static final long serialVersionUID = 1L;
+
+	JPanel panelW, panelWT, panelET, panelE, panelEN, panelEC, panelES, jp1, jp2;
+	JButton btnPay, btnPhonePay, btnList, btnMember; // 결제완료 버튼(로그인 페이지로 돌아감)
+	JTextArea area;
+	public OrderUI() {
+		init();
+		//assembly();
+	}
+	
+	public void assembly() {
+		
+	}
+
+	public void init(){
+		this.setTitle("주문정보확인");
+		//pullMember = new PullMember(); // 주문한 회원 정보를 끌어다오는 객체 *만들어야함
+		panelW = new JPanel(new BorderLayout());
+		panelWT = new JPanel();
+		panelE = new JPanel(new BorderLayout());
+		panelET = new JPanel(new BorderLayout());
+		panelEN = new JPanel();
+		panelES = new JPanel();
+		panelEC = new JPanel();
+		jp1 = new JPanel();
+		jp2 = new JPanel();
+		btnPay = new JButton("결제하기");
+		btnPhonePay = new JButton("휴대폰으로 결제하기");
+		btnList = new JButton("주문내역");
+		btnMember = new JButton("배송정보");
+		
+		
+		//===============assembly()=====================
+		//panelEC.setPreferredSize(new Dimension(200 ,100)); //jpanel크기 조절
+		
+		
+		panelEN.add(btnList);
+		panelEC.add(btnPhonePay);
+		panelES.add(btnPay);
+		
+		panelEN.setPreferredSize(new Dimension(700, 600));
+		panelEC.setPreferredSize(new Dimension(700, 100));
+		panelES.setPreferredSize(new Dimension(700, 100));
+		
+		panelW.setPreferredSize(new Dimension(700, 800));
+		panelW.setLayout(new BorderLayout());
+		
+		panelET.add(panelEN, "North");
+		panelET.add(panelEC, "Center");
+		panelET.add(panelES, "South");
+		panelWT.add(btnMember);
+		
+		panelE.add(panelET);
+		panelW.add(panelWT);
+		this.getContentPane().add(panelE, "East");
+		this.getContentPane().add(panelW, "West");
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(100, 100, 1400, 800); // 표시위치, 화면크기.
+		
+		this.setVisible(true);
+	}
+	
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
