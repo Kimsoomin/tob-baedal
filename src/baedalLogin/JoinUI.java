e164a93eb238a6b92b31e51ef5987e97fd1ef736
Hong-Hyeong-Dong <redmoonck9@gmail.com>
2015-10-27 17:01:38 +0900

    2015 10 27 / 5 : 00 dong

--- src/baedalLogin/JoinUI.java
+++ src/baedalLogin/JoinUI.java
@@ -1,178 +1,162 @@
 package baedalLogin;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import java.awt.Font;
-import javax.swing.border.BevelBorder;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
-public class JoinUI extends JFrame {
-	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField txtUserid;
-	private JTextField txtPassword;
-	private JTextField txtName;
-	private JTextField txtNum;
-	private JTextField txtAddr;
-	private JTextField txtHint;
-	private JTextField txtAnswer;
-	private JTextField txtBirth;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					JoinUI frame = new JoinUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	public JoinUI(){
-      		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    		setBounds(100, 100, 305, 573);
-    		contentPane = new JPanel();
-    		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-    		setContentPane(contentPane);
-    		contentPane.setLayout(null);
-    		
-    		JPanel panel = new JPanel();
-    		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-    		panel.setBounds(0, 0, 289, 535);
-    		contentPane.add(panel);
-    		panel.setLayout(null);
-    	 
-    		textField = new JTextField();
-    		textField.setEnabled(false);
-    		textField.setEditable(false);
-    		textField.setFont(new Font("굴림체", Font.BOLD, 15));
-    		textField.setText("\uD68C \uC6D0 \uAC00 \uC785");
-    		textField.setBounds(97, 21, 116, 31);
-    		panel.add(textField);
-    		textField.setColumns(10);
-    		
-    		txtUserid = new JTextField();
-    		txtUserid.setEnabled(false);
-    		txtUserid.setEditable(false);
-    		txtUserid.setText("  ID :");
-    		txtUserid.setBounds(51, 69, 33, 21);
-    		panel.add(txtUserid);
-    		txtUserid.setColumns(10);
-    		
-    		txtUserid = new JTextField();
-    		txtUserid.setBounds(123, 69, 116, 21);
-    		panel.add(txtUserid);
-    		txtUserid.setColumns(10);
-    		
-    		JButton btnIDCheck = new JButton("중복체크");
-    		btnIDCheck.setBounds(123, 100, 116, 23);
-    		panel.add(btnIDCheck);
-    		
-    		txtPassword = new JTextField();
-    		txtPassword.setText("    PW :");
-    		txtPassword.setEnabled(false);
-    		txtPassword.setEditable(false);
-    		txtPassword.setColumns(10);
-    		txtPassword.setBounds(51, 133, 33, 21);
-    		panel.add(txtPassword);
-    		
-    		txtPassword = new JTextField();
-    		txtPassword.setColumns(10);
-    		txtPassword.setBounds(123, 133, 116, 21);
-    		panel.add(txtPassword);
-    		
-    		txtName = new JTextField();
-    		txtName.setText("  Name :");
-    		txtName.setEnabled(false);
-    		txtName.setEditable(false);
-    		txtName.setColumns(10);
-    		txtName.setBounds(51, 172, 45, 21);
-    		panel.add(txtName);
-    		
-    		txtName = new JTextField();
-    		txtName.setColumns(10);
-    		txtName.setBounds(123, 172, 116, 21);
-    		panel.add(txtName);
-    		
-    		txtNum = new JTextField();
-    		txtNum.setText("  Numb :");
-    		txtNum.setEnabled(false);
-    		txtNum.setEditable(false);
-    		txtNum.setColumns(10);
-    		txtNum.setBounds(51, 206, 45, 21);
-    		panel.add(txtNum);
-    		
-    		txtNum = new JTextField();
-    		txtNum.setColumns(10);
-    		txtNum.setBounds(123, 206, 116, 21);
-    		panel.add(txtNum);
-    		
-    		txtAddr = new JTextField();
-    		txtAddr.setText("  Addr :");
-    		txtAddr.setEnabled(false);
-    		txtAddr.setEditable(false);
-    		txtAddr.setColumns(10);
-    		txtAddr.setBounds(51, 273, 51, 21);
-    		panel.add(txtAddr);
-    		
-    		txtAddr = new JTextField();
-    		txtAddr.setColumns(10);
-    		txtAddr.setBounds(123, 273, 116, 21);
-    		panel.add(txtAddr);
-    		
-    		txtHint = new JTextField();
-    		txtHint.setText("  Hint :");
-    		txtHint.setEnabled(false);
-    		txtHint.setEditable(false);
-    		txtHint.setColumns(10);
-    		txtHint.setBounds(51, 311, 33, 21);
-    		panel.add(txtHint);
-    		
-    		txtHint = new JTextField();
-    		txtHint.setColumns(10);
-    		txtHint.setBounds(123, 311, 116, 21);
-    		panel.add(txtHint);
-    		
-    		txtAnswer = new JTextField();
-    		txtAnswer.setText("  Answ :");
-    		txtAnswer.setEnabled(false);
-    		txtAnswer.setEditable(false);
-    		txtAnswer.setColumns(10);
-    		txtAnswer.setBounds(51, 347, 51, 21);
-    		panel.add(txtAnswer);
-    		
-    		txtAnswer = new JTextField();
-    		txtAnswer.setColumns(10);
-    		txtAnswer.setBounds(123, 347, 116, 21);
-    		panel.add(txtAnswer);
-    		
-    		txtBirth = new JTextField();
-    		txtBirth.setText("Birth :");
-    		txtBirth.setEnabled(false);
-    		txtBirth.setEditable(false);
-    		txtBirth.setColumns(10);
-    		txtBirth.setBounds(51, 387, 45, 21);
-    		panel.add(txtBirth);
-    		
-    		txtBirth = new JTextField();
-    		txtBirth.setColumns(10);
-    		txtBirth.setBounds(123, 387, 116, 21);
-    		panel.add(txtBirth);
-    		
-    		JButton btnJoin = new JButton("\uAC00\uC785");
-    		btnJoin.setBounds(51, 465, 73, 23);
-    		panel.add(btnJoin);
-    		
-    		JButton btnCancel = new JButton("\uCDE8\uC18C");
-    		btnCancel.setBounds(166, 465, 73, 23);
-    		panel.add(btnCancel);
-      	}
-} 
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class JoinUI extends JFrame implements ActionListener{
+   public static void main(String[] args) {
+      JoinUI join = new JoinUI();
+     
+   }
+   private static final long serialVersionUID = 1L;
+   Canvas canvas;
+   JPanel jp,jp1, jp2, jpId,jpIde,jpPw,jpName,jpBirth,jpAddr,jpHp,jpQue,jpAn,jpJo,jpCn;
+   JButton btnIde, btnJo,btnCn;
+   JLabel lbl1,lblId,lblPw,lblName,lblBirth,lblAddr,lblHp,lblQue,lblAn;
+   JTextField txtId,txtPw,txtName,txtBirth,txtAddr,txtHp,txtQue,txtAn;
+   JComboBox combo;
+
+   public JoinUI() {
+      this.setTitle("배달의기수");
+      lbl1 = new JLabel("회원가입");
+      lblId = new JLabel("ID");
+      lblPw = new JLabel("PW");
+      lblName = new JLabel("이름");
+      lblBirth = new JLabel("생년월일");
+      lblAddr = new JLabel("주소");
+      lblHp = new JLabel("핸드폰");
+      lblQue = new JLabel("질문");
+      lblAn = new JLabel("답변");
+      txtId = new JTextField(11);
+      txtPw = new JTextField(11);
+      txtName = new JTextField(11);
+      txtBirth = new JTextField(11);
+      txtAddr = new JTextField(11);
+      txtHp = new JTextField(11);
+      txtQue = new JTextField(11);
+      txtAn = new JTextField(11);
+      btnIde = new JButton("중복체크");
+      btnJo = new JButton("가입");
+      btnCn = new JButton("취소");
+      jp = new JPanel();
+      jp2 = new JPanel();
+      jp1 = new JPanel();
+      jpId = new JPanel();
+      jpIde = new JPanel();
+      jpPw = new JPanel();
+      jpName = new JPanel();
+      jpBirth = new JPanel();     
+      jpAddr = new JPanel();
+      jpHp = new JPanel();
+      jpQue = new JPanel();
+      jpAn = new JPanel();
+      jpJo= new JPanel();
+      jpCn = new JPanel();
+      jp.setLayout(new GridLayout(10, 1));
+      combo = new JComboBox();
+      jp1.add(lbl1);
+      jpId.add(lblId);                                   
+      jpId.add(txtId);
+      jpIde.add(btnIde);
+      jpPw.add(lblPw);
+      jpPw.add(txtPw);
+      jpName.add(lblName);
+      jpName.add(txtName);
+      jpBirth.add(lblBirth);
+      jpBirth.add(txtBirth);
+      jpAddr.add(lblAddr);
+      jpAddr.add(txtAddr);
+      jpHp.add(lblHp);
+      jpHp.add(txtHp);
+      jpQue.add(lblQue);
+      jpQue.add(combo);
+      combo.addItem("첫 강아지");     
+      combo.addItem("첫 학교");     
+      combo.addItem("첫 사랑");     
+      combo.addItem("태어난곳");     
+      combo.addItem("첫친구");
+      combo.setEditable(true);
+      jpAn.add(lblAn);
+      jpAn.add(txtAn);
+      jpJo.add(btnJo);
+      jpCn.add(btnCn);
+      jp.add(jp1);
+      jp.add(jpId);
+      jp.add(jpIde);
+      jp.add(jpPw);
+      jp.add(jpName);
+      jp.add(jpBirth);
+      jp.add(jpAddr);
+      jp.add(jpHp);
+      jp.add(jpQue);
+      jp.add(jpAn);
+      jp2.add(jpJo);
+      jp2.add(jpCn);
+     
+      btnIde.addActionListener(this);
+      btnJo.addActionListener(this);
+      btnCn.addActionListener(this);
+      combo.addActionListener(this);
+     
+      this.getContentPane().add(jp, "North");
+      this.getContentPane().add(jp2, "South");
+     
+     
+      this.add(jp);
+     
+      this.setBounds(100,50,400,700);
+      this.setVisible(true);
+     
+     
+   }
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      JComboBox cb = (JComboBox) e.getSource();
+      String qs = (String) e.getSource();
+      updateLabel(qs);
+      String command = e.getActionCommand();
+      switch (command) {
+      case "충복체크":
+        
+         break;
+      case "가입":
+        
+         break;
+      case "취소": System.exit(0);;
+         break;
+        
+
+      default:
+         break;
+      }
+   }
+   protected void updateLabel(String qs){
+      switch (qs) {
+      case "첫 강아지":
+         break;
+      case "첫 학교":
+         break;
+      case "첫 사랑":
+         break;
+      case "태어난곳":
+         break;
+      case "첫친구":
+         break;
+
+      default:
+         break;
+      }
+   }
+}
--- src/baedalLogin/LoginUI.java
+++ src/baedalLogin/LoginUI.java
@@ -1,72 +1,180 @@
 package baedalLogin;
-
-import java.awt.BorderLayout;
-import java.awt.Canvas;
-
-
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Vector;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
+public class LoginUI extends JFrame implements ActionListener{
+   Connection con;
+   PreparedStatement pstmt;
+   Timestamp reg_date;
 
-public class LoginUI extends JFrame implements ActionListener {
-      	public static void main(String[] args) {
-			LoginUI login = new LoginUI();
-			
-      	}
-      	private static final long serialVersionUID = 1L;  // 직렬화
-      	JLabel lblUserid = new JLabel("아이디");
-  		JLabel lblPassword = new JLabel("비밀번호");
-  		JTextField txtUserid = new JTextField(20);
-  		JTextField txtPassword = new JTextField(20);
-  		JButton btnLogin = new JButton("로그인");
-  		JTextArea area;
-      	
-      	public LoginUI() {
-      	JPanel jp = new JPanel(); 
-      	jp.setSize(200,200); // width, height
-      	jp.add(lblUserid,"North");
-      	jp.add(txtUserid);
-      	jp.add(lblPassword,"South");
-      	jp.add(txtPassword);
-      	jp.add(btnLogin);
-      	this.add(jp);
-      	this.setResizable(true); // 창의 크기를 변경시키는 것이 불가함
-      	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프레임에 닫기 버튼 장착
-      	this.setBounds(200,200,700,400);
-      	this.setVisible(true);
-		}
-      	
+   private String names[] = { "아이디", "비밀번호" };
+   private JLabel labels[];
+   private JTextField fields[];
+   private JButton login, join, search;
+   private JPanel panelCenter, panelSouth;
+   private int size = 2;
+   
+   public LoginUI(){
+    super("배달의 기수");   
+   
+      labels = new JLabel[ size ];
+      fields = new JTextField[ size ];
+      for ( int i = 0; i < labels.length; i++ )
+         labels[ i ] = new JLabel( names[ i ] );
+            
+      for ( int i = 0; i < fields.length; i++ )
+         fields[ i ] = new JTextField();
+      panelCenter = new JPanel();
+      panelCenter.setLayout( new GridLayout( size, 2 ) );
+      for ( int i = 0; i < size; i++ ) {
+         panelCenter.add( labels[ i ] );
+         panelCenter.add( fields[ i ] );
+      }
+      
+      login = new JButton("로그인");
+      join = new JButton("회원가입");      
+      search = new JButton("ID / PW 찾기");      
+      panelSouth = new JPanel();      
+      panelSouth.add( login );
+      panelSouth.add( join);
+      panelSouth.add( search );
+      add( panelCenter, "Center" );
+      add( panelSouth, "South" );     
+      setBounds(300,300,370,250);
+      setVisible(true);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      login.addActionListener(this);
+      join.addActionListener(this);
+      search.addActionListener(this);
 
-		public void actionPerformed1(ActionEvent e) {
-			String command = e.getActionCommand();
-      		switch (command) {
-      		case "입장":this.connect();break;
-      		
+   }//생성자 end
 
-      		default:break;
-			
-		}
-  	
- }
-
-		private void connect() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+   //Database 연결 부분
+   public void dbConnect(){ 
+     try{
+   Class.forName("");
+  }catch(ClassNotFoundException cnfe){
+   cnfe.printStackTrace();
+   System.out.println("드라이버 로딩에 실패");
+  }
+  try{
+   String url = "";
+         String userId = ""; 
+         String userPass = "";          
+        
+         con =DriverManager.getConnection(url,userId,userPass);
+  }catch(SQLException e){
+   e.printStackTrace();
+   System.out.println("커넥션 설정에 실패");
+  }
+   }
+   
+   // Event 처리 부분
+   public void actionPerformed(ActionEvent ae){
+       if(ae.getSource() == search){
+        dbConnect();
+        try {
+          idSelect();
+  } catch (SQLException e) {
+   e.printStackTrace();
+  }
+       }
+    else if(ae.getSource() == join){      // 회원 등록
+   dbConnect();
+   insertMember();
+         clearFields();         
+  }
+   }
+   public void idSelect() throws SQLException{
+    String UsrId = null;
+    ResultSet rs = null;
+    int Cnt = 0;
+    UsrId = fields[0].getText();
+    String SQL1 = "SELECT * FROM MEM02 WHERE ID='"+ UsrId +"'";
+    pstmt = con.prepareStatement(SQL1);
+    rs = pstmt.executeQuery();
+    while(rs.next()){
+     Cnt++;
+    }
+    
+    if(Cnt > 0){
+     JOptionPane.showMessageDialog(this,"중복된 아이디가 존재 합니다.");
+    }else{
+     JOptionPane.showMessageDialog(this,"아이디 중복검사가 완료되었습니다.\n중복된 아이디가 없습니다.");
+    }
+   }
+   
+   //회원 가입 처리 부분
+   public void insertMember(){
+       Timestamp reg_date = new Timestamp(System.currentTimeMillis());
+    String data[] = getFieldValues();
+// if(data[0].equals("") || data[1].equals("") || data[2].equals("") || data[3].equals("")){
+ if(fields[0].getText().equals("") ||
+    fields[1].getText().equals("") ||
+    fields[2].getText().equals("") ||
+    fields[3].getText().equals("") ){
+    JOptionPane.showMessageDialog(this,"모든 정보를 입력 하세요!");
+ }else if(fields[1].getText().equals(fields[2].getText())){
+//  }else if(data[1].equals(data[2])){ //비밀 번호가 일치하면 query문 실행
+    
+    String sql = "insert into mem02 values(?,?,?,?)";
+      
+       try{
+    pstmt = con.prepareStatement(sql);
+//          pstmt.setString(1,data[0]);  //아이디
+//          pstmt.setString(2,data[1]);  //비밀번호
+//          pstmt.setString(3,data[3]);  //이름
+    
+          pstmt.setString(1,fields[0].getText());  //아이디
+          pstmt.setString(2,fields[1].getText());  //비밀번호
+          pstmt.setString(3,fields[3].getText());  //이름
+          
+          pstmt.setTimestamp(4, reg_date); //회원가입 날짜
+    int result = pstmt.executeUpdate();    
+    if(result == 1){
+     JOptionPane.showMessageDialog(this,"회원 가입 완료");
+//     this.dispose();    
+    // MemberManagement management= new MemberManagement();
+    }else{
+     JOptionPane.showMessageDialog(this,"회원 가입 실패");
+    }
+    
+       }catch(SQLException e){
+   e.printStackTrace();
+   System.out.println("새로운 레코드 추가에 실패");
+  }
+  }else{ //비밀 번호가 일치하지 않으면 메시지 박스
+  JOptionPane.showMessageDialog(this,"비밀번호가 일치하지 않습니다.");
+  }
+   }  
+   
+   // 다시 작성 처리 부분
+   public void clearFields(){
+      for( int i = 0; i < size; i++ ){
+         fields[ i ].setText("");
+   }
+   }
+   // 입력한 회원 정보값을 구하는 부분
+   public String[] getFieldValues(){ 
+      String values[] = new String[ size ];
+      for ( int i = 0; i < size; i++ ) 
+         values[ i ] = fields[ i ].getText();
+      return values;
+   }
+   
+   public static void main(String[] args){
+	   LoginUI con= new LoginUI();
+   }
 }
