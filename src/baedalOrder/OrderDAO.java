aaf755abc984108e1f19a06b2bd8226a4812f9dd
HB <HB@HB-PC>
2015-10-28 14:45:46 +0900

    fd

--- src/baedalOrder/OrderDAO.java
+++ src/baedalOrder/OrderDAO.java
@@ -1,11 +1,19 @@
 package baedalOrder;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.List;
+
 
 import global.DAO;
 
 public class OrderDAO extends DAO{
-
+	private Connection con;
+	private Statement stmt;
+	private ResultSet res;
+	
+	
 	@Override
 	public void selectOrderMember() {
 		// TODO Auto-generated method stub
@@ -14,7 +22,7 @@ public class OrderDAO extends DAO{
 
 	@Override
 	public void selectOrderMenu() {
-		// TODO Auto-generated method stub
+	
 		
 	}
 
